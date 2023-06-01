c6e70420957ddfaf6f23b17c476cd11a38fd3aa0
Fahaddada47 <fifahad007@gmail.com>
2023-06-01 22:54:14 +0600

    Simple Quiz app develop by Flutter

---
+++ lib/constant.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color correct = Color(0xFF32AB58);
+const Color incorrect = Color(0xFFDC1313);
+const Color neutral = Color(0xFFB7AFB2);
+const Color background = Color(0xFF050A41);
+
---
+++ lib/model/question.dart
@@ -0,0 +1,19 @@
+class Question{
+  final String id;
+  final String title;
+  final Map<String, bool> option;
+  Question({
+    required this.id,
+    required this.title,
+    required this.option,
+
+});
+@override
+  String toString() {
+   return 'Question(id: $id, title: $title, option: $option)';
+  }
+
+
+
+
+}
